ab65d2f53944ececd80873c9f4af55875f8e6779
devOTTO <yoongotto@gmail.com>
2020-01-28 15:59:49 +0900

    Modifed .gitignore

---
+++ WebContent/index.jsp
@@ -0,0 +1,23 @@
+<%
+  session.setAttribute("a","a");
+%>
+<html>
+<head>
+<title>TOMCAT1</title>
+</head>
+
+<body>
+<h1>Was1</h1>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr bgcolor="#CCCCCC">
+    <td width="13%">TomcatB Machine</td>
+    <td width="87%">&nbsp;</td>
+  </tr>
+  <tr>
+    <td>Session ID :</td>
+    <td><%=session.getId()%></td>
+  </tr>
+</table>
+</body>
+</html>
+
---
+++ src/web/MovieServlet.java
@@ -0,0 +1,57 @@
+package web;
+
+import domain.Movie;
+import domain.MovieService;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import util.Status;
+
+@WebServlet("/showmoviedata")
+public class MovieServlet extends HttpServlet {
+
+    public void doPost(HttpServletRequest request,
+            HttpServletResponse response)
+            throws IOException, ServletException {
+        processRequest(request, response);
+    }
+
+    public void processRequest(HttpServletRequest request,
+            HttpServletResponse response)
+            throws IOException, ServletException {
+        RequestDispatcher view = null;
+        Status status = new Status();
+        request.setAttribute("status", status);
+
+        int movId = Integer.parseInt(request.getParameter("movId"));
+
+        Movie movie = null;
+
+        try {
+
+            MovieService movieservice = new MovieService();
+
+            movie = movieservice.selectedMovie(movId);
+
+            if (!status.isSuccessful()) {
+                view = request.getRequestDispatcher("MovieList.jsp");
+                view.forward(request, response);
+                return;
+            }
+
+            request.setAttribute("movie", movie);
+
+            view = request.getRequestDispatcher("MovieData.jsp");
+            view.forward(request, response);
+
+        } catch (Exception e) {
+            status.addException(e);
+            view = request.getRequestDispatcher("MovieList.jsp");
+            view.forward(request, response);
+        }
+    }
+}
