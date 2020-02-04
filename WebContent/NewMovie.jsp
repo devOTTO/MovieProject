00a84aca08fb5db85fd0b67516e74228f229258e
devOTTO <yoongotto@gmail.com>
2020-02-04 17:13:47 +0900

    Add create director autocomplete

--- WebContent/NewMovie.jsp
+++ WebContent/NewMovie.jsp
@@ -1,10 +1,49 @@
 ​<%@page session="false" import="java.util.Iterator"%>
 <jsp:useBean id="status" scope="request" class="util.Status"/>
+<%@page import="domain.DirectorService"%>
+<%@page import="domain.Director"%>
 <%@page session="false" language="java" contentType="text/html; charset=UTF-8"
         pageEncoding="UTF-8" %>
 <!DOCTYPE html>
 <html>
    <%@ include file="common/header.jsp" %> 
+
+<script type="text/javascript">
+$(document).ready(function() {
+	 $("#dirId").autocomplete({
+	 source : function(request, response) {
+	 $.ajax({
+	 url : "/MovieProject/searchdirector",
+	 type : "post",
+	 dataType : "json",
+	 data: {dirName:$("#dirId").val()},
+	 success : function(data) {
+		 response(
+			$.map(data, function(item) {
+				return {
+					label: item.dirName,
+					value: item.dirId
+				}
+			})
+		)},
+	 error : function(xhr, status, error) {
+		 console.log(error);
+		 alert(error);
+	}
+	 });},
+	 minLength: 2,
+	 response: function(event, ui){
+		 console.log(ui);
+	 },
+	 select: function(event, ui){
+		 console.log("Selected:" + ui.item.value);
+	 },
+	 focus : function(event, ui) {    //포커스 가면
+         return false;//한글 에러 잡기용도로 사용됨
+     },
+	 });
+	});
+</script>
     <body>
     	<%@ include file="common/title.jsp" %>  
         <h2 class="col-md-6">Create Movie Form</h2>
@@ -25,11 +64,11 @@
 		  </div>
 		  <div class="form-group">
 		    <label for="dirAge">Movie Rating</label>
-		    <input type="text" class="form-control" name="movRate" placeholder=">Movie Rating">
+		    <input type="text" class="form-control" name="movRate" placeholder="Movie Rating">
 		  </div>
 		  <div class="form-group">
-		    <label for="dirAge">Director ID</label>
-		    <input type="text" class="form-control" name="dirId" placeholder="Director ID">
+		    <label for="dirId">Director Id (Search By Id)</label>
+		    <input type="text" class="form-control" id="dirId" name="dirId" placeholder="Search Director Name...">
 		  </div>
 		  <button type="submit" class="btn btn-primary">Submit</button>
         </form>
---
+++ src/web/SearchDirectorServlet.java
@@ -0,0 +1,70 @@
+package web;
+
+import domain.Director;
+import domain.DirectorService;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import util.Status;
+
+@WebServlet("/searchdirector")
+public class SearchDirectorServlet extends HttpServlet {
+	
+
+    public void doPost(HttpServletRequest request,
+            HttpServletResponse response)
+            throws IOException, ServletException {
+        processRequest(request, response);
+    }
+
+    public void processRequest(HttpServletRequest request,
+            HttpServletResponse response)
+            throws IOException, ServletException {
+        RequestDispatcher view = null;
+        Status status = new Status();
+        request.setAttribute("status", status);
+        request.setCharacterEncoding("UTF-8");
+        String dirName = request.getParameter("dirName");
+
+        ArrayList<Director> directors = new ArrayList<Director>();
+
+        try {
+
+            DirectorService directorservice = new DirectorService();
+
+            directors = directorservice.getSearchedDirectors(dirName);
+            StringBuffer result = new StringBuffer("");
+            result.append("[");
+            //result.append("{\"result\":[");
+            for(int i = 0; i<directors.size(); ++i) {
+            	result.append("{\"dirId\":" + directors.get(i).getDirId() + ",");
+            	result.append("\"dirName\":\"" + directors.get(i).getDirName() + "\"}");
+            	if(i+1 != directors.size()) result.append(",");
+            }
+            result.append("]");
+     
+            if (!status.isSuccessful()) {
+                view = request.getRequestDispatcher("DirectorList.jsp");
+                view.forward(request, response);
+                return;
+            }
+            //response.setContentType("text/html;charset=UTF-8");
+            response.setContentType("application/json");
+            response.getWriter().print(result);
+            response.getWriter().flush();
+            System.out.println(result.toString());
+            //view = request.getRequestDispatcher("DirectorData.jsp");
+            //view.forward(request, response);
+
+        } catch (Exception e) {
+            status.addException(e);
+            //view = request.getRequestDispatcher("DirectorList.jsp");
+            //view.forward(request, response);
+        }
+    }
+}
